d0e16b9894b7281f1116f18f4aa0bc5f7516d66f
Max McDonough <max.mcdonough@gmail.com>
2023-07-26 22:04:35 -0400

    Working data logic and display results

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Search from './components/Search/Search';
 import { AppBar, AppBarTitle } from '@react-md/app-bar';
 import './App.css'
@@ -12,13 +12,18 @@ interface CountryData {
   capital: string;
   population: number;
   flagUrl: string;
+  flagAlt: string;
   coatOfArmsUrl: string;
 }
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [countryData, setCountryData] = useState<CountryData | null>(null); 
+  const [countryData, setCountryData] = useState<CountryData | null>(null);
+
+  useEffect(() => {
+    console.log(countryData);
+  }, [countryData]);
 
   return (
     <>
@@ -28,10 +33,25 @@ const App = () => {
       <Search
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
-        // countryData={countryData}
-        // setCountryData={setCountryData}
+        countryData={countryData}
+        setCountryData={setCountryData}
       />
-      <p>Search term: {searchTerm}</p>
+      {countryData 
+        ?
+          <main>
+            <p>Common Name: {countryData.commonName}</p>
+            <p>Official Name: {countryData.officialName}</p>
+            <p>Currency: {countryData.currency}</p>
+            <p>Currency Symbol: {countryData.currencySymbol}</p>
+            <p>Language: {countryData.language}</p>
+            <p>Capital: {countryData.capital}</p>
+            <p>Population: {countryData.population}</p>
+            <img src={countryData.flagUrl} alt={countryData.flagAlt} />
+            <img src={countryData.coatOfArmsUrl} alt={countryData.officialName} />
+          </main>
+        :  
+          <p>No country data found</p>
+      }
     </>
   )
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -12,10 +12,11 @@ interface CountryData {
   capital: string;
   population: number;
   flagUrl: string;
+  flagAlt: string;
   coatOfArmsUrl: string;
 }
 
-const Search = ({searchTerm, setSearchTerm, countryData, setCountryData} : {searchTerm: string, setSearchTerm: Dispatch<SetStateAction<string>>, countryData: CountryData, setCountryData: Dispatch<SetStateAction<CountryData>>}) => { 
+const Search = ({searchTerm, setSearchTerm, countryData, setCountryData} : {searchTerm: string, setSearchTerm: Dispatch<SetStateAction<string>>, countryData: CountryData | null, setCountryData: Dispatch<SetStateAction<CountryData | null>>}) => { 
 
   const searchRef = useRef(null);
 
@@ -27,15 +28,59 @@ const Search = ({searchTerm, setSearchTerm, countryData, setCountryData} : {sear
       try {
         const result = await getCountryData(searchTerm);
         console.log(result);
+
+        // Get country name, population, flag url, and flag alt
         const { 
           name: {common},
-          name: {offical},
-          //  currencySymbol, language, capital, population, flagUrl, coatOfArmsUrl
+          name: {official},
+          population,
+          flags: {png},
+          flags: {alt}, 
         } = result[0];
+        const commonName = common;
+        const officialName = official;
+        const flagUrl = png;
+        const flagAlt = alt;
+
+        // Get currency name and symbol
         const currencyObject = result[0].currencies;
         const currencyNames = Object.keys(currencyObject);
         const arrayOfCurrencies = currencyNames.map(currency => ({key: currency, value: currencyObject[currency]}));
-        console.log(arrayOfCurrencies);
+        const {
+          value: {name},
+          value: {symbol}
+        } = arrayOfCurrencies[0];
+        const currency = name;
+        const currencySymbol = symbol;
+
+        // Get language
+        const languageObject = result[0].languages;
+        const languageNames = Object.keys(languageObject);
+        const arrayOfLanguages = languageNames.map(language => ({key: language, value: languageObject[language]}));
+        const {
+          value
+        } = arrayOfLanguages[0];
+        const language = value;
+
+        // Get capital
+        const capital = result[0].capital[0];
+
+        // Get coat of arms
+        const coatOfArms = result[0].coatOfArms.png;
+
+        // Set CoutnryData state
+        setCountryData({
+          commonName: commonName,
+          officialName: officialName,
+          currency: currency,
+          currencySymbol: currencySymbol,
+          language: language,
+          capital: capital,
+          population: population,
+          flagUrl: flagUrl,
+          flagAlt: flagAlt,
+          coatOfArmsUrl: coatOfArms
+        });
 
       } catch (err) {
         console.log(err);
